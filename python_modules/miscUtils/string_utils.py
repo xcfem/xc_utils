14ac7e3fb989524b8a76baa548523bc4c406e87a
Luis <l.pereztato@gmail.com>
2016-04-14 17:59:56 +0200

    Added some small string utilities.

---
+++ python_modules/miscUtils/string_utils.py
@@ -0,0 +1,45 @@
+import re
+import unicodedata
+
+def remove_accents(input_str):
+    nfkd_form = unicodedata.normalize('NFKD', input_str)
+    only_ascii = nfkd_form.encode('ASCII', 'ignore')
+    return only_ascii
+
+def slugify(s):
+    """
+    Simplifies ugly strings into something URL-friendly.
+    >>> print slugify("[Some] _ Article's Title--")
+    some-articles-title
+    """
+
+    # "[Some] _ Article's Title--"
+    # "[some] _ article's title--"
+    s = s.lower()
+
+    # "[some] _ article's_title--"
+    # "[some]___article's_title__"
+    for c in [' ', '-', '.', '/']:
+        s = s.replace(c, '_')
+
+    # "[some]___article's_title__"
+    # "some___articles_title__"
+    s = re.sub('\W', '', s)
+
+    # "some___articles_title__"
+    # "some   articles title  "
+    s = s.replace('_', ' ')
+
+    # "some   articles title  "
+    # "some articles title "
+    s = re.sub('\s+', ' ', s)
+
+    # "some articles title "
+    # "some articles title"
+    s = s.strip()
+
+    # "some articles title"
+    # "some-articles-title"
+    s = s.replace(' ', '-')
+
+    return s
