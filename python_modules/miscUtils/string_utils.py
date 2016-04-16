48f57ff9dd915295c5517ffaddd84159210bcae5
Luis <l.pereztato@gmail.com>
2016-04-16 18:25:39 +0200

    Workaround to remove accents in strings in string_utils.py

--- python_modules/miscUtils/string_utils.py
+++ python_modules/miscUtils/string_utils.py
@@ -3,7 +3,7 @@ import re
 import unicodedata
 
 def remove_accents(input_str):
-    nfkd_form = unicodedata.normalize('NFKD', unicode(input_str))
+    nfkd_form = unicodedata.normalize('NFKD', unicode(input_str,'utf-8'))
     only_ascii = nfkd_form.encode('ASCII', 'ignore')
     return only_ascii
 
