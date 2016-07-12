2d0ecb0d848180f646a3b363736283755d6a3b34
Luis <l.pereztato@gmail.com>
2016-07-12 13:14:17 +0200

    Adding a bit of documentation.

--- python_modules/geom_utils/section_by_plane.py
+++ python_modules/geom_utils/section_by_plane.py
@@ -4,6 +4,7 @@ import xc_base
 import geom
 
 class SectionByPlane(object):
+  '''Intersection of a polyline or a list of polylines by a plane.'''
   name= "-"
   plane= geom.Plano3d(geom.Pos3d(1,0,0), geom.Pos3d(0,1,0), geom.Pos3d(0,0,1))
 
@@ -12,9 +13,11 @@ class SectionByPlane(object):
     self.plane= p
 
   def getIntersection(self,pline):
+    '''Returns the intersection of the polyline with the plane.'''
     return pline.getIntersection(self.plane)
 
   def getIntersectionPoints(self,listOfPlines):
+    '''Returns the intersection of a list of polylines with the plane.'''
     retval= geom.Polilinea3d()
     for pline in listOfPlines:
       lp= pline.getIntersection(self.plane)
