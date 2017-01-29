c4960ced162f5cc49ebb528a5a21d8d03362cbb3
Luis C. Pérez Tato <l.pereztato@gmail.com>
2017-01-29 20:37:37 +0100

    Class translated into English.

---
+++ src/geom/d2/Triang3dMesh.cc
@@ -0,0 +1,155 @@
+//----------------------------------------------------------------------------
+//  biblioteca xc_utils; bilioteca de comandos para el intérprete del lenguaje
+//  de entrada de datos.
+//
+//  Copyright (C)  Luis Claudio Pérez Tato
+//
+//  Este software es libre: usted puede redistribuirlo y/o modificarlo 
+//  bajo los términos de la Licencia Pública General GNU publicada 
+//  por la Fundación para el Software Libre, ya sea la versión 3 
+//  de la Licencia, o (a su elección) cualquier versión posterior.
+//
+//  Este software se distribuye con la esperanza de que sea útil, pero 
+//  SIN GARANTÍA ALGUNA; ni siquiera la garantía implícita 
+//  MERCANTIL o de APTITUD PARA UN PROPÓSITO DETERMINADO. 
+//  Consulte los detalles de la Licencia Pública General GNU para obtener 
+//  una información más detallada. 
+//
+// Debería haber recibido una copia de la Licencia Pública General GNU 
+// junto a este programa. 
+// En caso contrario, consulte <http://www.gnu.org/licenses/>.
+//----------------------------------------------------------------------------
+//Triang3dMesh.cc
+
+#include "Triang3dMesh.h"
+#include "xc_utils/src/geom/d2/Triangulo3d.h"
+#include "Triedro3d.h"
+#include "xc_utils/src/gnu_gts/GTSSurface.h"
+#include "xc_utils/src/gnu_gts/GTSEdge.h"
+#include "xc_utils/src/gnu_gts/TriangleMap.h"
+
+
+template <class HDS>
+class Build_poliedro: public CGAL::Modifier_base<HDS>
+  {
+    const GTSSurface &sf;
+  public:
+    Build_poliedro(const GTSSurface &gts_surf)
+      : sf(gts_surf) {}
+    void operator()( HDS& hds)
+      {
+        const size_t num_facetas= sf.GetNumCaras();
+        const size_t num_aristas= sf.GetNumAristas();
+        const size_t num_vertices= sf.GetNumVertices();
+        TriangleMap mt= sf.GetTriangleMap();
+        // Postcondition: `hds' is a valid polyhedral surface.
+        CGAL::Polyhedron_incremental_builder_3<HDS> B( hds, true);
+        B.begin_surface(num_vertices,num_facetas,num_aristas);
+        const VerticesMap &vertices= mt.getVertices();
+        for(VerticesMap::const_iterator i= vertices.begin();
+            i!= vertices.end();i++)
+          {
+            const Pos3d coo_vertice= (*i).second;
+            B.add_vertex(coo_vertice.ToCGAL());        
+          }
+        const TriangleFaces &triangles= mt.getFaces();
+        for(TriangleFaces::const_iterator j= triangles.begin();
+            j!=triangles.end();j++)
+          {
+            B.begin_facet();
+            B.add_vertex_to_facet((*j).V1()-1);
+            B.add_vertex_to_facet((*j).V2()-1);
+            B.add_vertex_to_facet((*j).V3()-1);
+            B.end_facet();
+          }
+        B.end_surface();
+      }
+  };
+
+Triang3dMesh::Triang3dMesh(const GTSSurface &gts_surf)
+  {
+    typedef CGPoliedro_3::HalfedgeDS HalfedgeDS;
+    Build_poliedro<HalfedgeDS> bpoli(gts_surf);
+    cgpoliedro.delegate(bpoli);
+    //CGAL_assertion( P.is_triangle( P.halfedges_begin()));
+  }
+
+Triang3dMesh GTSSurface2Triang3dMesh(const GTSSurface &gts_surf)
+  { return Triang3dMesh(gts_surf); }
+GTSSurface Triang3dMesh::get_gts_surface(void) const
+  {
+     GTSSurface retval;
+//     Triang3dMesh tmp= Triangula();
+//     typedef MapPoligonos<CGTriangMesh_3> map_poligonos;
+//     map_poligonos map= GetMapPoligonos();
+//     deque<GTSVertex> vertices;
+//     for(map_poligonos::vertices_const_iterator i= map.VerticesBegin();
+//         i!= map.VerticesEnd(); i++)
+//       {
+//         Pos3d pos= (*i).second;
+//         GTSVertex v(pos.x(),pos.y(),pos.z());
+//         vertices.push_back(v);
+//       }
+//     typedef pair<size_t,size_t> verts_arista;
+//     typedef set<verts_arista > set_aristas;
+//     set_aristas s_aristas;
+//     deque<GTSEdge> aristas;
+//     for(map_poligonos::caras_const_iterator i= map.CarasBegin();
+//         i!= map.CarasEnd(); i++)
+//       {
+//         const size_t v1= (*i).Vertice(1);
+//         const size_t v2= (*i).Vertice(2);
+//         const size_t v3= (*i).Vertice(3);
+//         const verts_arista lado1(v1,v2);
+//         if(s_aristas.find(lado1)== s_aristas.end()) //Arista nueva.
+//           {
+// 	    s_aristas.insert(lado1);
+//             aristas.push_back(GTSEdge(vertices[lado1.first],vertices[lado1.second]));
+//           }
+//         const verts_arista lado2(v2,v3);
+//         if(s_aristas.find(lado2)== s_aristas.end()) //Arista nueva.
+//           {
+// 	    s_aristas.insert(lado2);
+//             aristas.push_back(GTSEdge(vertices[lado2.first],vertices[lado2.second]));
+//           }
+//         const verts_arista lado3(v3,v1);
+//         if(s_aristas.find(lado3)== s_aristas.end()) //Arista nueva.
+//           {
+// 	    s_aristas.insert(lado3);
+//             aristas.push_back(GTSEdge(vertices[lado3.first],vertices[lado3.second]));
+//           }
+//         GTSEdge l1= XXX
+//       }
+    
+    std::cerr << "Triang3dMesh::get_gts_surface no implementada." << std::endl;
+    return retval;
+  }
+
+Triangulo3d Triang3dMesh::GetTrianguloCara(const Facet_const_iterator &f) const
+  {
+    Facet::Halfedge_const_handle h = f->halfedge();
+    return Triangulo3d(Pos3d(h->vertex()->point()),
+                       Pos3d(h->next()->vertex()->point()),
+                       Pos3d(h->next()->next()->vertex()->point()));
+  }
+Triang3dMesh::Facet_const_iterator Triang3dMesh::busca_triedro(const Pos3d &org,const Pos3d &p,const double &tol) const
+  //Busca el triedro formado por el punto org y una de las facetas
+  //que contiene al punto p que se pasa como parámetro.
+  {
+    const size_t nf= GetNumCaras();
+    Facet_const_iterator retval= facets_end();
+    if(nf<4) return retval;
+    for(Facet_const_iterator i= facets_begin();i!=facets_end();i++)
+      {
+        Triangulo3d tf(GetTrianguloCara(i));
+        Triedro3d triedro(org,tf.Vertice(1),tf.Vertice(2),tf.Vertice(3));
+        if(triedro.In(p,tol))
+          {
+            retval= i;
+            break;
+          }
+      }
+    return retval;
+  }
+Triang3dMesh::Facet_const_iterator Triang3dMesh::BuscaTriedro(const Pos3d &org,const Pos3d &p,const double &tol) const
+   { return busca_triedro(org,p,tol); }
---
+++ src/geom/d2/Triang3dMesh.h
@@ -0,0 +1,87 @@
+//----------------------------------------------------------------------------
+//  biblioteca xc_utils; bilioteca de comandos para el intérprete del lenguaje
+//  de entrada de datos.
+//
+//  Copyright (C)  Luis Claudio Pérez Tato
+//
+//  Este software es libre: usted puede redistribuirlo y/o modificarlo 
+//  bajo los términos de la Licencia Pública General GNU publicada 
+//  por la Fundación para el Software Libre, ya sea la versión 3 
+//  de la Licencia, o (a su elección) cualquier versión posterior.
+//
+//  Este software se distribuye con la esperanza de que sea útil, pero 
+//  SIN GARANTÍA ALGUNA; ni siquiera la garantía implícita 
+//  MERCANTIL o de APTITUD PARA UN PROPÓSITO DETERMINADO. 
+//  Consulte los detalles de la Licencia Pública General GNU para obtener 
+//  una información más detallada. 
+//
+// Debería haber recibido una copia de la Licencia Pública General GNU 
+// junto a este programa. 
+// En caso contrario, consulte <http://www.gnu.org/licenses/>.
+//----------------------------------------------------------------------------
+//Triang3dMesh.h
+
+#ifndef TRIANG3D_MESH_H
+#define TRIANG3D_MESH_H
+
+#include "../d3/poliedros3d/Poliedro3d.h"
+
+class Triangulo3d;
+class SoGroup;
+class GTSSurface;
+class Pos3d;
+
+
+//! @ingroup GEOM
+//
+//! @brief 3D Triange mesh.
+class Triang3dMesh: public Poliedro3d
+  {
+    GTSSurface get_gts_surface(void) const;
+
+    friend class Poliedro3d;
+
+  public:
+    typedef Poliedro3d::Vertex Vertex;
+    typedef Poliedro3d::Facet Facet;
+    typedef Poliedro3d::Point_iterator Point_iterator;
+    typedef Poliedro3d::Point_const_iterator Point_const_iterator;
+    typedef Poliedro3d::Vertex_iterator Vertex_iterator;
+    typedef Poliedro3d::Vertex_const_iterator Vertex_const_iterator;
+    typedef Poliedro3d::Facet_iterator Facet_iterator;
+    typedef Poliedro3d::Facet_const_iterator Facet_const_iterator;
+    typedef Poliedro3d::Edge_iterator Edge_iterator;
+    typedef Poliedro3d::Halfedge_iterator Halfedge_iterator;
+    typedef Poliedro3d::Halfedge_around_facet_circulator Halfedge_around_facet_circulator;
+    typedef Poliedro3d::Halfedge_around_vertex_circulator Halfedge_around_vertex_circulator;
+    typedef Poliedro3d::Vertex_handle Vertex_handle;
+    typedef Poliedro3d::Facet_handle Facet_handle;
+    typedef Poliedro3d::Halfedge_handle Halfedge_handle;
+
+  protected:
+    Facet_const_iterator busca_triedro(const Pos3d &org,const Pos3d &p,const double &tol) const;
+
+  public:
+
+    Triang3dMesh(void): Poliedro3d() {}
+    explicit Triang3dMesh(const Poliedro3d &p)
+      : Poliedro3d(p) {}
+    Triang3dMesh(const GTSSurface &gts_surf);
+    Triang3dMesh(const Triang3dMesh &otro) 
+      : Poliedro3d(otro) {}
+    Triang3dMesh &operator=(const Triang3dMesh &otro)
+      {
+	Poliedro3d::operator=(otro);
+        return *this;
+      }
+    virtual GeomObj *clon(void) const
+      { return new Triang3dMesh(*this); }
+
+    Triangulo3d GetTrianguloCara(const Facet_const_iterator &f) const;
+
+    Facet_const_iterator BuscaTriedro(const Pos3d &org,const Pos3d &p,const double &tol) const;
+
+    friend Triang3dMesh GTSSurface2Triang3dMesh(const GTSSurface &gts_surf);
+  };
+
+#endif
