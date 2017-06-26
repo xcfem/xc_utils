0ae9076e5dae7fdf2654d83104499c8abb736b23
Luis C. Pérez Tato <l.pereztato@gmail.com>
2017-06-26 18:15:41 +0200

    Functions to make easier the use of Mononobe-Okabe method.

--- python_modules/geotecnia/FrictionalSoil.py
+++ python_modules/geotecnia/FrictionalSoil.py
@@ -2,6 +2,7 @@
 from __future__ import division
 import math
 from scipy.constants import g
+from geotecnia import mononobe_okabe
 
 '''FrictionalSoil.py: frictional (or cohesionless) soil model.'''
 
@@ -32,6 +33,10 @@ class FrictionalSoil(object):
   def K0Jaky(self):
     '''Returns Jaky's earth pressure at rest.'''
     return 1.0-math.sin(self.getDesignPhi())
+  def Ka(self):
+    '''Passive earth pressure coefficient.'''
+    sinPhi= math.sin(self.getDesignPhi())
+    return (1-sinPhi)/(1+sinPhi)
   def Kp(self):
     '''Passive earth pressure coefficient.'''
     sinPhi= math.sin(self.getDesignPhi())
@@ -42,3 +47,20 @@ class FrictionalSoil(object):
   def getDesignPhi(self):
     '''Return the design value of the soil internal friction angle.'''
     return self.phi/self.gammaMPhi
+  def getMononobeOkabeDryOverpressure(self,H,kv,kh,psi= math.radians(90),delta_ad= 0.0,beta= 0.0,Kas= None,g= 9.81):
+    ''' Overpressure due to seismic action according to Mononobe-Okabe
+
+        Args:
+        :H: height of the structure.
+        :kv: seismic coefficient of vertical acceleration.
+        :kh: seismic coefficient of horizontal acceleration.
+        :psi: back face inclination of the structure (< PI/2)
+        :beta: slope inclination of backfill.
+        :delta_ad: angle of friction soil - structure.
+        :Kas: static earth pressure coefficient 
+    '''
+    gamma_soil= self.rho*g
+    phi_d= math.atan(math.tan(self.phi)/1.25)
+    if(Kas==None):
+      Kas= self.Ka()
+    return mononobe_okabe.overpressure_dry(H, gamma_soil, kv, kh, psi, phi_d, delta_ad,beta,Kas)
