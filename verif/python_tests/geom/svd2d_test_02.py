ff98f34e39f1775e38a65548042250440fb0fa87
Luis C. Pérez Tato <l.pereztato@gmail.com>
2018-02-09 09:36:37 +0100

    Linear combination of sliding vector systems verification test.

---
+++ verif/python_tests/geom/svd2d_test_02.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+import xc_base
+import geom
+import math
+
+svd1=geom.SVD2d()
+ptoAplic=geom.Pos2d(1,1)
+vectorDir=geom.Vector2d(1,0)
+vec=geom.VDesliz2d(ptoAplic,vectorDir)
+svd1+=vec
+
+svd2=geom.SVD2d()
+ptoAplic=geom.Pos2d(1,1)
+vectorDir=geom.Vector2d(0.5,0)
+vec=geom.VDesliz2d(ptoAplic,vectorDir)
+svd2+=vec
+
+R= 5.0*svd1-10.0*svd2
+result= R.getResultante()
+mom= R.getMomento()
+ratio= result.getModulo()+ math.sqrt(mom**2)
+
+# print 'R= ', R
+# print 'result= ', result
+# print 'mom= ', mom
+# print 'ratio= ', ratio
+
+import os
+fname= os.path.basename(__file__)
+if(ratio<1e-13):
+  print "test ",fname,": ok."
+else:
+  print "test ",fname,": ERROR."
+
