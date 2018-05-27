ff4130f3903d2a7a426ce42f746e205bfb24a57d
Luis C. Pérez Tato <l.pereztato@gmail.com>
2018-05-27 11:29:36 +0200

    English translation.

--- python_modules/geotecnia/cimientos/long_elastica_pilote.py
+++ python_modules/geotecnia/cimientos/long_elastica_pilote.py
@@ -2,16 +2,16 @@ import math
 def longElasticaPiloteArcillas(Epilote,Ipilote,Eterreno,L):
     '''
     longElasticaPiloteArcillas(Epilote,Ipilote,Eterreno,L):
-    Longitud elástica del pilote en arcilla
+    Elastic lenght of the pile in clay
     '''
     LePA=math.pow(Epilote*Ipilote/(Eterreno/3.),0.25)
     if LePA>2*L:
-        print "El pilote es demasiado corto para su longitud elástica."
+        print "Pile is too short to compute its elastic lenght."
     return(LePA)
 
 def longEmpotramientoPiloteArcillas(Epilote,Ipilote,Eterreno,L):
     '''
     longEmpotramientoPiloteArcillas(Epilote,Ipilote,Eterreno,L): 
-    Longitud de empotramiento del pilote en arcilla
+    Anchorage length of the pile in clay
     '''
     return(1.2*longElasticaPiloteArcillas(Epilote,Ipilote,Eterreno,L))
