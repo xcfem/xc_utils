f869a04639ca4a15b0ad0095b1bed4c82d2b67ac
Luis C. Pérez Tato <l.pereztato@gmail.com>
2018-12-02 13:13:35 +0100

    Added matrix of boolean class (row colum retrieval stuff).

---
+++ src/matrices/m_bool.h
@@ -0,0 +1,33 @@
+//----------------------------------------------------------------------------
+//  xc_utils library; general purpose classes and functions.
+//
+//  Copyright (C)  Luis Claudio Pérez Tato
+//
+//  Este software es libre: usted puede redistribuirlo y/o modificarlo 
+//  bajo los términos de la Licencia Pública General GNU publicada 
+//  por la Fundación para el Software Libre, ya sea la versión 3 
+//  de la Licencia, o (a su elección) cualquier versión posterior.
+//
+//  Este software se distribuye con la esperanza de que sea útil, pero 
+//  SIN GARANTÍA ALGUNA; ni siquiera la garantía implícita 
+//  MERCANTIL o de APTITUD PARA UN PROPÓSITO DETERMINADO. 
+//  Consulte los detalles de la Licencia Pública General GNU para obtener 
+//  una información más detallada. 
+//
+// Debería haber recibido una copia de la Licencia Pública General GNU 
+// junto a este programa. 
+// En caso contrario, consulte <http://www.gnu.org/licenses/>.
+//----------------------------------------------------------------------------
+//m_bool.h
+
+#ifndef M_BOOL_H
+#define M_BOOL_H
+
+#include <vector>
+#include "TMatrix.h"
+
+typedef std::vector<bool> std_vector_bool;
+
+typedef TMatrix<bool,std_vector_bool > m_bool;
+
+#endif
