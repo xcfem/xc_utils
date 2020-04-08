497ca95641a2d9bf7659f3127d6f672dcd2a94c5
Luis C. Pérez Tato <l.pereztato@gmail.com>
2020-04-08 10:29:08 +0200

    Minor debugging

--- verif/python_tests/soil_mechanics/test_mononobe_okabe.py
+++ verif/python_tests/soil_mechanics/test_mononobe_okabe.py
@@ -1,6 +1,14 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import division
+from __future__ import print_function
+
+__author__= "Luis C. Pérez Tato (LCPT)"
+__copyright__= "Copyright 2016, LCPT"
+__license__= "GPL"
+__version__= "3.0"
+__email__= "l.pereztato@gmail.com"
+
 import xc_base
 import geom
 import math
